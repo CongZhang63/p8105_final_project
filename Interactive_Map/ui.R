61d45921ad1b5df33f443513459c033b0a020ac3
Zhourong Li <zl2977@cumc.columbia.edu>
2020-12-01 20:46:12 +0800

    Added map attributes.

--- Interactive_Map/ui.R
+++ Interactive_Map/ui.R
@@ -10,3 +10,34 @@ library(shinydashboard)
 winedata=read_csv("./wine_first150k.csv.csv",locale = readr::locale(encoding = "UTF-8"))
 lat_long=read_csv("./latitude_and_longitude_values.csv")
 continent=read_csv("./continent.csv")
+
+lat_long=subset(lat_long, 
+                select = -c(usa_state_code,
+                            usa_state_latitude,
+                            usa_state_longitude,
+                            usa_state)
+)
+
+winedata$continent <- countrycode(sourcevar = winedata$country,
+                                  origin = "country.name",
+                                  destination = "continent")
+
+winedata =
+  winedata %>%
+  mutate(country  = ifelse(country == "US","United States", country))
+
+wine_location=merge(winedata, lat_long, by.x = "country", by.y = "country" )
+wine_location = arrange(wine_location,X1)
+
+
+continent_choice = wine_location %>% distinct(continent) %>% pull()
+continent_choice[6] ="All Continents"
+
+
+max_price = wine_location %>% distinct(price) %>% max(na.rm = T)
+min_price = wine_location %>% distinct(price) %>% min(na.rm = T)
+
+
+rating = wine_location %>% distinct(points) %>% pull()
+rating=sort(rating, decreasing = T)
+rating=prepend(rating, "All Ratings")
